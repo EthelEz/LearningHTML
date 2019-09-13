8753fc469bdc9f7bfbd3acee5444bd917538d28d
EthelEz <nedu47@gmail.com>
2019-09-13 18:49:33 +0100

    Updated Learning

---
+++ LearningJave/ConnectingJava.js
@@ -0,0 +1,7 @@
+var greetings = "Welcome to bankPHB, Mr: "
+var name = prompt(name)
+// alert(greetings + name)
+
+var deposit = prompt("How much do you intend to deposit today?")
+alert("You've deposited: "+deposit)
+console.log("You try, your money don finish?")
---
+++ LearningJave/Part3.js
@@ -0,0 +1,59 @@
+ var lbs = prompt("What is the weight in pounds (lbs)?")
+var kg = lbs*0.454
+alert("That is:  "+kg+" kilograms")
+console.log("Conversion Completed")
+
+
+// var hot = false
+// var temp = 60
+//
+// if (temp<80) {
+//   console.log("hot outside!")
+// }else{
+//   console.log("Its not very hot today!")
+// }
+
+// // console.log(hot)
+// while (temp<80) {
+//   console.log("Yes its true eh")
+// }
+// else {
+//   return 0
+// }
+// for (var i = 0; i < 5; i=i+1) {
+//   console.log("Number is "+i)
+// }
+
+// var word = "ABCDEFGHIJK"
+// for (var i = 0; i < word.length; i++) {
+//   console.log(word[i]);
+// }
+
+// var esc = "ABABABABABABABABAB"
+//
+// for (var i = 0; i < esc.length; i=i+2) {
+//   console.log(esc[i])
+// }
+// var h = 0
+// while (h<5) {
+//   console.log("hello");
+//   h=h+1
+// }
+//
+// for (var i = 0; i < 5; i++) {
+//   console.log("Kello")
+// }
+
+for (var i = 0; i < 25; i++) {
+  if (x%2 !==0) {
+    console.log(x);
+  }
+}
+
+var x = 1;
+while (x<25) {
+  if (x%2 !==0) {
+  console.log(x);
+  }
+  x++
+}
